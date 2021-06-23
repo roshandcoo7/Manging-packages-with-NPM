1322371b0942685b18ce0b31a8aa8b0eead32199
Roshan <mdroshan@thesolarlabs.com>
2021-06-23 22:53:05 +0530

    Commenting out server processes

--- server.js
+++ server.js
@@ -3,57 +3,57 @@
  * the verification process may break
  * ***************************************************/
 
-'use strict';
+// 'use strict';
 
-var fs = require('fs');
+// var fs = require('fs');
 var bGround = require('fcc-express-bground');
 var express = require('express');
 var myApp = require('./myApp');
 var app = express();
 
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
+// if (!process.env.DISABLE_XORIGIN) {
+//   app.use(function(req, res, next) {
+//     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+//     var origin = req.headers.origin || '*';
+//     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+//          console.log(origin);
+//          res.setHeader('Access-Control-Allow-Origin', origin);
+//          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     }
+//     next();
+//   });
+// }
 
-app.use('/public', express.static(process.cwd() + '/public'));
+// app.use('/public', express.static(process.cwd() + '/public'));
 
-app.route('/_api/package.json')
-  .get(function(req, res, next) {
-    console.log('requested');
-    fs.readFile(__dirname + '/package.json', function(err, data) {
-      if(err) return next(err);
-      res.type('txt').send(data.toString());
-    });
-  });
+// app.route('/_api/package.json')
+//   .get(function(req, res, next) {
+//     console.log('requested');
+//     fs.readFile(__dirname + '/package.json', function(err, data) {
+//       if(err) return next(err);
+//       res.type('txt').send(data.toString());
+//     });
+//   });
   
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
+// app.route('/')
+//     .get(function(req, res) {
+// 		  res.sendFile(process.cwd() + '/views/index.html');
+//     })
 
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
-});
+// // Respond not found to all the wrong routes
+// app.use(function(req, res, next){
+//   res.status(404);
+//   res.type('txt').send('Not found');
+// });
 
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
-})
+// // Error Middleware
+// app.use(function(err, req, res, next) {
+//   if(err) {
+//     res.status(err.status || 500)
+//       .type('txt')
+//       .send(err.message || 'SERVER ERROR');
+//   }  
+// })
 
 var port = process.env.PORT || 5000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
