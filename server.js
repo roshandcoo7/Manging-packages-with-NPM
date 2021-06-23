7c7b2ec89168239f093a18c11bf99dec70e668ec
Roshan <mdroshan@thesolarlabs.com>
2021-06-23 18:18:18 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(function(err, req, res, next) {
   }  
 })
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Node.js listening ...');
 });
 
